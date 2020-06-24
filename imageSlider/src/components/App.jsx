7732c86211bc7ce4387e98d7aec3592d5ab9c180
Jessica Morgan <jmorgan@aucklandmuseum.com>
2020-06-25 10:58:22 +1200

    image slider styling

--- imageSlider/src/components/App.jsx
+++ imageSlider/src/components/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import Slider from "react-slick"
+import Slider from 'react-slick'
 import ReactCompareImage from 'react-compare-image'
 
-const imageData = window.__IMAGE_DATA__ || [];
-const pageData = window.__PAGE_DATA__ || [];
+const imageData = window.__GALLERY_DATA__ || [];
 
 class App extends React.Component {
   constructor(props) {
@@ -12,14 +11,15 @@ class App extends React.Component {
         imageId: 0
     }
   }
-
+  
   afterChangeHandler = (e) => {
     this.setState({imageId: e})
   }
 
 render () {
 
-  const currentImageText = imageData.find(image => image.id === this.state.imageId)
+
+const currentImageText = imageData.items.find(image => image.id === this.state.imageId)
 
   const settings = {
     dots: true,
@@ -34,28 +34,33 @@ render () {
 
 
   return ( 
-    <div className='container'> 
-    <h1 dangerouslySetInnerHTML={{ __html: pageData.title }}/>
+    <div id="image-slider" className="image-slider-container"> 
+    <h1 dangerouslySetInnerHTML={{ __html: imageData.title }}/>
     <br/>
      <Slider {...settings}>
-      {imageData.map((img) => {
+      {imageData.items.map((img) => {
         return <div>
-         <ReactCompareImage id={img.id} leftImage={img.image1} rightImage={img.image2} sliderLineColor='rgba(255, 255, 255, 0.6)' sliderLineWidth={3} style={{boxShadow: 'none'}} handle={<img src="arrows.png" alt="arrows" style={{width: 30, height: 15, opacity: 0.65}}/>}/>
+         <ReactCompareImage id={img.id} leftImage={img.image1.original} rightImage={img.image2.original} sliderLineColor='rgba(255, 255, 255, 0.6)' sliderLineWidth={3} style={{boxShadow: 'none'}} handle={<img src="https://www.aucklandmuseum.com/getmedia/9f57c5a4-b04c-41bf-920e-f65e48671020/arrows" alt="arrows" style={{width: 30, height: 15, opacity: 0.65}}/>}/>
          </div>
       }
      )}
 
      </Slider>
-     <div className='text-grid'>
-    <div className='col1'><p className='image-title' dangerouslySetInnerHTML={{ __html: currentImageText.title1 }}/><p id="date" className='date' dangerouslySetInnerHTML={{ __html: currentImageText.date1 }}/></div>
-    <div className="caption1" dangerouslySetInnerHTML={{ __html: currentImageText.caption1}}/>
-    <div className="credit1" dangerouslySetInnerHTML={{ __html: currentImageText.credit1 }}/>
-    <div className="link1" dangerouslySetInnerHTML={{ __html: currentImageText.link1 }}/>
+     
+    <div className="text-grid">
+    <div className="col1"><p className="image-title" dangerouslySetInnerHTML={{ __html: currentImageText.image1.title }}/><p id="date" className="date" dangerouslySetInnerHTML={{ __html: currentImageText.image1.text }}/></div>
+    <p className="caption1" dangerouslySetInnerHTML={{ __html: currentImageText.image1.caption}}/> 
+    {/* <div style={{borderBottom: '1px solid #000', gridColumn: 1, gridRow: 3}} /> */}
+    <p className="credit1" dangerouslySetInnerHTML={{ __html: currentImageText.image1.credit }}/>   
+    <p className="copyright1" dangerouslySetInnerHTML={{ __html: currentImageText.image1.copyright }}/>
+    <p className="link1" dangerouslySetInnerHTML={{ __html: currentImageText.image1.source }}/>
     <br/>
-    <div className='col2'><p className="image-title" dangerouslySetInnerHTML={{ __html: currentImageText.title2 }}/><p id="date" className='date' dangerouslySetInnerHTML={{ __html: currentImageText.date2 }}/></div>
-    <div className="caption2" dangerouslySetInnerHTML={{ __html: currentImageText.caption2}}/>
-    <div className="credit2" dangerouslySetInnerHTML={{ __html: currentImageText.credit2 }}/>
-    <div className="link2" dangerouslySetInnerHTML={{ __html: currentImageText.link2 }}/>
+    <div className="col2"><p className="image-title" dangerouslySetInnerHTML={{ __html: currentImageText.image2.title }}/><p id="date" className="date" dangerouslySetInnerHTML={{ __html: currentImageText.image2.text }}/></div>
+    <p className="caption2" dangerouslySetInnerHTML={{ __html: currentImageText.image2.caption}}/>
+    {/* <div style={{borderBottom: '1px solid #000', gridColumn: 2, gridRow: 3}} /> */}
+    <p className="credit2" dangerouslySetInnerHTML={{ __html: currentImageText.image2.credit }}/>
+    <p className="copyright2" dangerouslySetInnerHTML={{ __html: currentImageText.image2.copyright }}/>
+    <p className="link2" dangerouslySetInnerHTML={{ __html: currentImageText.image2.source }}/>
    </div>
    </div>
  
@@ -63,3 +68,4 @@ render () {
 }
 
 export default App
+
--- imageSlider/src/index.js
+++ imageSlider/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './components/App'
-
 import './main.css'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
     <App />,
-    document.getElementById('root')
+    document.getElementById('app')
   )
 })
